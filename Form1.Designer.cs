786faaf6a15bbb03c65cc5c63cba1444a9d4abd6
godspeed-ai <125993906+godspeed-ai@users.noreply.github.com>
2025-05-26 00:57:21 +0800

    RTF格式
    修改程式碼

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,23 +37,26 @@
             this.btnRedo = new System.Windows.Forms.Button();
             this.btnUndo = new System.Windows.Forms.Button();
             this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            this.comboBoxSize = new System.Windows.Forms.ComboBox();
+            this.comboBoxStyle = new System.Windows.Forms.ComboBox();
+            this.comboBoxFont = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // btnOpen
             // 
-            this.btnOpen.Location = new System.Drawing.Point(37, 12);
+            this.btnOpen.Location = new System.Drawing.Point(574, 44);
             this.btnOpen.Name = "btnOpen";
-            this.btnOpen.Size = new System.Drawing.Size(73, 34);
+            this.btnOpen.Size = new System.Drawing.Size(100, 34);
             this.btnOpen.TabIndex = 0;
             this.btnOpen.Text = "開啟檔案";
             this.btnOpen.UseVisualStyleBackColor = true;
-            this.btnOpen.Click += new System.EventHandler(this.button1_Click);
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(126, 12);
+            this.btnSave.Location = new System.Drawing.Point(680, 44);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(73, 34);
+            this.btnSave.Size = new System.Drawing.Size(96, 34);
             this.btnSave.TabIndex = 1;
             this.btnSave.Text = "存檔";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -66,6 +69,7 @@
             this.rtbText.Size = new System.Drawing.Size(497, 381);
             this.rtbText.TabIndex = 2;
             this.rtbText.Text = "";
+            this.rtbText.TextChanged += new System.EventHandler(this.rtbText_TextChanged);
             // 
             // openFileDialog1
             // 
@@ -75,14 +79,14 @@
             // 
             this.listUndo.FormattingEnabled = true;
             this.listUndo.ItemHeight = 12;
-            this.listUndo.Location = new System.Drawing.Point(567, 52);
+            this.listUndo.Location = new System.Drawing.Point(567, 124);
             this.listUndo.Name = "listUndo";
-            this.listUndo.Size = new System.Drawing.Size(209, 376);
+            this.listUndo.Size = new System.Drawing.Size(209, 304);
             this.listUndo.TabIndex = 3;
             // 
             // btnRedo
             // 
-            this.btnRedo.Location = new System.Drawing.Point(387, 12);
+            this.btnRedo.Location = new System.Drawing.Point(680, 84);
             this.btnRedo.Name = "btnRedo";
             this.btnRedo.Size = new System.Drawing.Size(96, 34);
             this.btnRedo.TabIndex = 4;
@@ -91,22 +95,49 @@
             // 
             // btnUndo
             // 
-            this.btnUndo.Location = new System.Drawing.Point(254, 12);
+            this.btnUndo.Location = new System.Drawing.Point(574, 84);
             this.btnUndo.Name = "btnUndo";
             this.btnUndo.Size = new System.Drawing.Size(100, 34);
             this.btnUndo.TabIndex = 5;
             this.btnUndo.Text = "回復上一步";
             this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
             // 
-            // saveFileDialog2
+            // comboBoxSize
             // 
-            this.saveFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog2_FileOk);
+            this.comboBoxSize.FormattingEnabled = true;
+            this.comboBoxSize.Location = new System.Drawing.Point(286, 20);
+            this.comboBoxSize.Name = "comboBoxSize";
+            this.comboBoxSize.Size = new System.Drawing.Size(85, 20);
+            this.comboBoxSize.TabIndex = 7;
+            this.comboBoxSize.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
+            // 
+            // comboBoxStyle
+            // 
+            this.comboBoxStyle.FormattingEnabled = true;
+            this.comboBoxStyle.Location = new System.Drawing.Point(413, 20);
+            this.comboBoxStyle.Name = "comboBoxStyle";
+            this.comboBoxStyle.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxStyle.TabIndex = 8;
+            this.comboBoxStyle.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
+            // 
+            // comboBoxFont
+            // 
+            this.comboBoxFont.FormattingEnabled = true;
+            this.comboBoxFont.Location = new System.Drawing.Point(59, 20);
+            this.comboBoxFont.Name = "comboBoxFont";
+            this.comboBoxFont.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxFont.TabIndex = 9;
+            this.comboBoxFont.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxFont);
+            this.Controls.Add(this.comboBoxStyle);
+            this.Controls.Add(this.comboBoxSize);
             this.Controls.Add(this.btnUndo);
             this.Controls.Add(this.btnRedo);
             this.Controls.Add(this.listUndo);
@@ -130,6 +161,9 @@
         private System.Windows.Forms.Button btnRedo;
         private System.Windows.Forms.Button btnUndo;
         private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+        private System.Windows.Forms.ComboBox comboBoxSize;
+        private System.Windows.Forms.ComboBox comboBoxStyle;
+        private System.Windows.Forms.ComboBox comboBoxFont;
     }
 }
 
